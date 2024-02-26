8456febded0edacf0f25ed854ab5c2f68a266282
Engreyxien <engreyxien@gmail.com>
2024-02-26 16:38:02 +0800

    practice

--- problem1.js
+++ problem1.js
@@ -1,6 +1,10 @@
 function countInstance(arr, char) {
   return arr.map((str) => str.split("").filter((c) => c === char).length);
 }
+//this code defines a function called countInstance that takes an array of strings and a character as arguments.
+//The map method is used to split each string into an array of characters.
+//The filter method is used to count the number of times the character appears in each string.
 
 const answer = countInstance(["luzon", "visayas", "mindanao"], "a");
+//The result is an array of numbers that represent the number of times the character appears in each string.
 console.log(answer);
--- problem2.js
+++ problem2.js
@@ -1,7 +1,11 @@
 function returnDivisibleBy3(arr) {
   return arr.map((num) => num * num + 2).filter((num) => num % 3 === 0);
 }
+//this code defines a function called returnDivisibleBy3 that takes an array of numbers as an argument.
+//The map method is used to multiply each number by itself and add 2.
+//The function returns an array of numbers that are divisible by 3 when multiplied by themselves plus 2.
+//The filter method is used to filter out the numbers that are not divisible by 3.
 
-const answer = returnDivisibleBy3([21, 11, 14, 34, 42, 56]);
-// console.log(answer) ---> "[3, 6, 18, 27]"
+const answer = returnDivisibleBy3([21, 11, -14, 34, 42, 56]);
+//The result is an array of numbers that are divisible by 3 when multiplied by themselves plus 2.
 console.log(answer);
